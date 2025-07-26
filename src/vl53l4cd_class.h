4d277c680b2d5d9df8f1867df1672725730263e6
Gabriel Dalboozi <gabriel.dalboozi@helsing.ai>
2025-07-26 12:49:00 +0100

    Correct header.

--- src/vl53l4cd_class.h
+++ src/vl53l4cd_class.h
@@ -506,7 +506,7 @@ class VL53L4CD {
     */
 
     //uint8_t VL53L4CD_I2CRead(uint8_t DeviceAddr, uint16_t RegisterAddress, uint8_t *p_values, uint32_t size);
-    uint8_t VL53L4CD_I2CReadBytesWithLimit(uint8_t DeviceAddr, uint16_t RegisterAddress, uint8_t *p_values);
+    uint8_t VL53L4CD_I2CReadBytesWithLimit(uint8_t DeviceAddr, uint16_t RegisterAddress, uint8_t *p_values, uint32_t size);
 
     /**
     * @brief Write multiple bytes through I2C.
